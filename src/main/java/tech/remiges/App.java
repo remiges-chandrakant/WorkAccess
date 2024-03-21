7c7a14b767ade3cc6367c288a264316cef74a919
chandrakant-remiges <chandrakant.gupta@remiges.tech>
2024-03-21 10:45:19 +0530

    project add

---
+++ src/main/java/tech/remiges/App.java
@@ -0,0 +1,14 @@
+package tech.remiges;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("you can change code and then commit.");
+        System.out.println("please change line or add new line");
+
+    }
+}
