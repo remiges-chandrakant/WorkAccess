37b9a8d32d1e34cbbb93c9026d8936c75282f75f
rajeshc <rajesh.chowdhury@remiges.tech>
2024-03-21 13:15:15 +0530

    code by raj

--- src/main/java/tech/remiges/App.java
+++ src/main/java/tech/remiges/App.java
@@ -10,6 +10,8 @@ public class App {
         System.out.println("you can change code and then commit.");
         System.out.println("please change line or add new line");
         System.out.println("Code changed by Suraj");
+        System.out.println("hello developer");
+        
 
     }
 
@@ -21,5 +23,8 @@ public class App {
     public String getName(){
       return "Shivendra";
     }
+    
+    public int getName(){
+    return 3;
 }
 
